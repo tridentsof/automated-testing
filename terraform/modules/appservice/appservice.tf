ac3a47e095c746c4b8dbabe2e0ff534d9ce594e3
Tri Dang <51437905+tridentsof@users.noreply.github.com>
2023-12-14 22:14:49 +0700

    init source code

---
+++ terraform/modules/appservice/appservice.tf
@@ -0,0 +1,25 @@
+resource "azurerm_service_plan" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  os_type             = "Windows"
+  sku_name            = "B1"
+}
+
+resource "azurerm_windows_web_app" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  service_plan_id     = azurerm_service_plan.test.id
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
+  }
+  site_config {
+    always_on = false
+    application_stack {
+      current_stack   = "dotnet"
+      dotnet_version  = "v4.0"
+    } 
+  }
+}
